7716a042161c37793b5341a6682c41c8b25c6bdf
WaqarAhmad707 <officialwaqar707@gmail.com>
2023-06-18 19:25:27 +0500

    add comments in carController

--- controllers/car/index.js
+++ controllers/car/index.js
@@ -1,99 +1,115 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Car = require('../../models/carModel');
-const { createCarSchema, updateCarSchema } = require('../../validations/carValidation');
+const Car = require("../../models/carModel");
+const {
+  createCarSchema,
+  updateCarSchema,
+} = require("../../validations/carValidation");
 
-// Registration API
-router.post('/', async (req, res) => {
-    // Get user data from the request body
-    const { category, color, model, make, registrationNo } = req.body;
+// Add Car API
+router.post("/", async (req, res) => {
+  // Get user data from the request body
+  const { category, color, model, make, registrationNo } = req.body;
 
-    try {
-        await createCarSchema.validateAsync(req.body);
+  try {
+    // checking JOI validations here
+    await createCarSchema.validateAsync(req.body);
 
-        const car = await Car.create({
-            category,
-            color,
-            model,
-            make,
-            registrationNo,
-          });
-        const carWithCategory = await Car.findById(car._id).populate('category', {name:1});
-        res.status(201).json({ car: carWithCategory });
-    } catch (error) {
-        if (error.isJoi) {
-            // Return error response if validation fails
-            return res.status(400).json({ error: error.details[0].message });
-        }
-        res.status(400).json({ message: error.message });
+    // saving car into DB
+    const car = await Car.create({
+      category,
+      color,
+      model,
+      make,
+      registrationNo,
+    });
+
+    // Getting newly created car with its category name
+    const carWithCategory = await Car.findById(car._id).populate("category", {
+      name: 1,
+    });
+
+    // Return success response
+    res.status(201).json({ car: carWithCategory });
+  } catch (error) {
+    if (error.isJoi) {
+      // Return error response if validation fails
+      return res.status(400).json({ error: error.details[0].message });
     }
+    res.status(400).json({ message: error.message });
+  }
 });
 
-// Login API
-router.get('/', async (req, res) => {
-    try {
-        const cars  = await Car.find().populate('category', {name:1});
-        res.status(200).json({ cars  });
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+// Get All Cars API
+router.get("/", async (req, res) => {
+  try {
+    const cars = await Car.find().populate("category", { name: 1 });
+    res.status(200).json({ cars });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
-router.get('/:id', async (req, res) => {
-    const carId = req.params.id;
-    try {
-        const car = await Car.findById(carId).populate('category', {name:1});
+// Get A Car By ID API
+router.get("/:id", async (req, res) => {
+  const carId = req.params.id;
+  try {
+    const car = await Car.findById(carId).populate("category", { name: 1 });
 
-        if (!car) {
-            return res.status(400).json({ message: 'Car not found' });
-        }
-        res.status(200).json({ car });
-    } catch (error) {
-        res.status(400).json({ message: error.message });
+    // checking is car not exist
+    if (!car) {
+      return res.status(400).json({ message: "Car not found" });
     }
+    res.status(200).json({ car });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
-router.put('/:id', async (req, res) => {
-    const carId = req.params.id;
-    const { category, color, model, make, registrationNo } = req.body;
+// Update A Car API
+router.put("/:id", async (req, res) => {
+  const carId = req.params.id;
+  const { category, color, model, make, registrationNo } = req.body;
 
-    try {
-        // Validate the request body against the updateCarSchema
-        await updateCarSchema.validateAsync(req.body);
+  try {
+    // Checking JOI validations
+    await updateCarSchema.validateAsync(req.body);
 
-        const car = await Car.findByIdAndUpdate(
-        carId,
-        { category, color, model, make, registrationNo },
-        { new: true }
-        ).populate('category', {name:1});
+    const car = await Car.findByIdAndUpdate(
+      carId,
+      { category, color, model, make, registrationNo },
+      { new: true }
+    ).populate("category", { name: 1 });
 
-        if (!car) {
-            return res.status(400).json({ message: 'Car not found' });
-        }
-
-        res.status(200).json({ car });
-    } catch (error) {
-        if (error.isJoi) {
-            // Return error response if validation fails
-            return res.status(400).json({ error: error.details[0].message });
-        }
-        res.status(400).json({ message: error.message });
+    if (!car) {
+      return res.status(400).json({ message: "Car not found" });
     }
+
+    res.status(200).json({ car });
+  } catch (error) {
+    if (error.isJoi) {
+      // Return error response if validation fails
+      return res.status(400).json({ error: error.details[0].message });
+    }
+    res.status(400).json({ message: error.message });
+  }
 });
 
-router.delete('/:id', async (req, res) => {
-    const carId = req.params.id;
-    try {
-        const car = await Car.findByIdAndRemove(carId);
+// Delete A Car API
+router.delete("/:id", async (req, res) => {
+  const carId = req.params.id;
+  try {
+    const car = await Car.findByIdAndRemove(carId);
 
-        if (!car) {
-            return res.status(404).json({ message: 'Car not found' });
-        }
-
-        res.status(200).json({ message: 'Car deleted' });
-    } catch (error) {
-        res.status(400).json({ message: error.message });
+    // checking car exists or not
+    if (!car) {
+      return res.status(404).json({ message: "Car not found" });
     }
+
+    res.status(200).json({ message: "Car deleted" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 module.exports = router;
