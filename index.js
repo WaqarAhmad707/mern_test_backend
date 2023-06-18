8dd70281a59839bedc322fc0404da085d8169984
WaqarAhmad707 <officialwaqar707@gmail.com>
2023-06-18 18:37:04 +0500

    setup up server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
