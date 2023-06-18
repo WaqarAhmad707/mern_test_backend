c4fc8e8b182cffc75bc1ab22cc8579e4b2b10b3a
WaqarAhmad707 <officialwaqar707@gmail.com>
2023-06-18 19:08:50 +0500

    add comments in index.js

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const cors = require('cors');
-require('dotenv').config();
-const { connectToDatabase } = require('./configs/dbConfig'); // Adjust the path if needed
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { connectToDatabase } = require("./configs/dbConfig");
+const authMiddleware = require("./middlewares/authMiddleware");
+
+// add .env config into app
+require("dotenv").config();
+
+// creating express app
 const app = express();
-const authMiddleware = require('./middlewares/authMiddleware');
 
-const port = process.env.PORT;
+// setting up port
+const port = process.env.PORT || 3001;
 
+// middlewares
 app.use(cors());
 app.use(bodyParser.json());
-connectToDatabase()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.use('/auth',require('./controllers/auth'))
-app.use('/category', authMiddleware, require('./controllers/category'))
-app.use('/car', authMiddleware, require('./controllers/car'))
+// connecting to DB
+connectToDatabase();
 
+// test endpoint
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// attach controllers with specified routes
+app.use("/auth", require("./controllers/auth"));
+app.use("/category", authMiddleware, require("./controllers/category"));
+app.use("/car", authMiddleware, require("./controllers/car"));
+
+// listener function
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
